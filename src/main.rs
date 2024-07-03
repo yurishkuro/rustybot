d6d879199d68ace7c9af63145910fdaa3e57008c
Yuri Shkuro <github@ysh.us>
2024-07-03 17:04:22 -0400

    Argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("rustybot")
+        .version("1.0")
+        .author("Yuri Shkurov")
+        .about("rustybot")
+        .arg(Arg::with_name("name")
+             .long("name")
+             .short("n")
+             .takes_value(true)
+             .default_value("World")
+             .help("Sets the name to greet"));
+    let matches = app.get_matches();
+    let name = matches.value_of("name").unwrap();
+    println!("Hello, {}!", name);
 }
