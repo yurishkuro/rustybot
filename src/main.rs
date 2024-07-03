5c906dc484ca89a76b036552e792cd1f83d81960
Yuri Shkuro <github@ysh.us>
2024-07-03 17:51:54 -0400

    Format

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ fn main() {
         .version("1.0")
         .author("Yuri Shkurov")
         .about("rustybot")
-        .arg(Arg::with_name("name")
-             .long("name")
-             .short("n")
-             .takes_value(true)
-             .default_value("World")
-             .help("Sets the name to greet"));
+        .arg(
+            Arg::with_name("name")
+                .long("name")
+                .short("n")
+                .takes_value(true)
+                .default_value("World")
+                .help("Sets the name to greet"),
+        );
     let matches = app.get_matches();
     let name = matches.value_of("name").unwrap();
     println!("Hello, {}!", name);
